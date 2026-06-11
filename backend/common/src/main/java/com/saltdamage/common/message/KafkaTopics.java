49c591dcd35f08d3e4be35fc4f70cc101c6c239b
fostdp <129711106+fostdp@users.noreply.github.com>
2026-06-11 20:49:25 +0800

    Update project

--- backend/common/src/main/java/com/saltdamage/common/message/KafkaTopics.java
+++ backend/common/src/main/java/com/saltdamage/common/message/KafkaTopics.java
@@ -8,6 +8,15 @@ public final class KafkaTopics {
     public static final String TOPIC_CRYSTALLIZATION_RESULT = "salt-damage-crystallization-result";
     public static final String TOPIC_ALARM_EVENT = "salt-damage-alarm-event";
 
+    public static final String TOPIC_CYCLE_COUNT_REQUEST = "salt-damage-cycle-count-request";
+    public static final String TOPIC_CYCLE_COUNT_RESULT = "salt-damage-cycle-count-result";
+    public static final String TOPIC_MICROCLIMATE_REQUEST = "salt-damage-microclimate-request";
+    public static final String TOPIC_MICROCLIMATE_RESULT = "salt-damage-microclimate-result";
+    public static final String TOPIC_DELAMINATION_REQUEST = "salt-damage-delamination-request";
+    public static final String TOPIC_DELAMINATION_RESULT = "salt-damage-delamination-result";
+    public static final String TOPIC_BLOCKCHAIN_STORE = "salt-damage-blockchain-store";
+    public static final String TOPIC_BLOCKCHAIN_RESULT = "salt-damage-blockchain-result";
+
     private KafkaTopics() {
     }
 }
